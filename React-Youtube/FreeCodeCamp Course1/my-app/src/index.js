d5785b7f1df41627bdc93c68b26b02b57004be53
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-01-07 15:48:43 +0200

    first react component

---
+++ React-Youtube/FreeCodeCamp Course1/my-app/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+function Greeting() {
+  return <h1>Its my first React Component</h1>;
+}
+
+ReactDOM.render(<Greeting />, document.getElementById('root'));
