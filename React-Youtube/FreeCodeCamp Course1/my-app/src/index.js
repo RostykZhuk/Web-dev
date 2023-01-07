86c5049f866cc0d21ebf3c1e07c89d34f0580e14
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-01-07 19:17:09 +0200

    component detailed

--- React-Youtube/FreeCodeCamp Course1/my-app/src/index.js
+++ React-Youtube/FreeCodeCamp Course1/my-app/src/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-function Greeting() {
-  return <h1>Its my first React Component!!!!</h1>;
-}
+
+// stateless functional component
+// always return jsx
+
+// function Greeting() {
+//   return (
+//     <div>
+//       <h1>Hello world</h1>
+//     </div>
+//   );
+// }
+// the same
+const Greeting = () => {
+  return React.createElement(
+    'div',
+    {},
+    React.createElement('h1', {}, 'hello world')
+  );
+};
 
 ReactDOM.render(<Greeting />, document.getElementById('root'));
