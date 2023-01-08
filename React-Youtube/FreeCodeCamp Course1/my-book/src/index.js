6650549b74f1cac8e07899e3be6a4779c4ca1c5b
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-01-08 17:36:30 +0200

    props children

--- React-Youtube/FreeCodeCamp Course1/my-book/src/index.js
+++ React-Youtube/FreeCodeCamp Course1/my-book/src/index.js
@@ -21,22 +21,31 @@ function BookList() {
         title={firstBook.title}
         author={firstBook.author}
       />
+
       <Book
         img={secondBook.img}
         title={secondBook.title}
         author={secondBook.author}
-      />
+      >
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Non ipsa aut
+          aperiam quod laudantium, consequatur in ea corporis, hic aliquid ex
+          eveniet tenetur dicta veniam cumque ut accusantium doloribus.
+          Possimus?
+        </p>
+      </Book>
     </section>
   );
 }
 
-const Book = ({ img, title, author }) => {
+const Book = ({ img, title, author, children }) => {
   // const { img, title, author } = props;
   return (
     <article className='book'>
       <img src={img} alt='' />
       <h1>{title}</h1>
       <h4>{author}</h4>
+      {children}
     </article>
   );
 };
