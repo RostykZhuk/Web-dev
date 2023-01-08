b1c1aa25d279edcf4abdbff7fa99682746d1ad57
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-01-08 16:45:29 +0200

    jsx javascript

--- React-Youtube/FreeCodeCamp Course1/my-book/src/index.js
+++ React-Youtube/FreeCodeCamp Course1/my-book/src/index.js
@@ -8,39 +8,24 @@ function BookList() {
   return (
     <section className='booklist'>
       <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
     </section>
   );
 }
 
+const author = 'Suzanne Collins';
 const Book = () => {
+  const title = 'The Hunger Games';
   return (
     <article className='book'>
-      <Image />
-      <Title />
-      <Author />
+      <img
+        src='https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1586722975l/2767052._SY475_.jpg'
+        alt=''
+      />
+      <h1>{title}</h1>
+      <h4>{author.toUpperCase()}</h4>
     </article>
   );
 };
-const Image = () => (
-  <img
-    src='https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1586722975l/2767052._SY475_.jpg'
-    alt=''
-  />
-);
-
-const Title = () => <h1>The Hunger Games</h1>;
-const Author = () => (
-  <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem' }}>
-    Suzanne Collins
-  </h4>
-);
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(<BookList />);
