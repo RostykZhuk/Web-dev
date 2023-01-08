7acabbd5c7cdb86f7b69ea74cc3f6ee079672f49
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-01-08 22:20:55 +0200

    onClick OnMouseOver

--- React-Youtube/FreeCodeCamp Course1/my-book/src/index.js
+++ React-Youtube/FreeCodeCamp Course1/my-book/src/index.js
@@ -35,13 +35,38 @@ function BookList() {
   );
 }
 
-const Book = (props) => {
-  const { img, title, author } = props;
+const Book = ({ img, title, author }) => {
+  // attribute, eventHandler
+  // onClick,onMouseOver
+  const clickHandler = () => {
+    console.log('Hello world');
+  };
+  const complexExample = (author) => {
+    console.log(author);
+  };
+
   return (
-    <article className='book'>
+    <article
+      className='book'
+      onMouseOver={() => {
+        console.log(title);
+      }}
+    >
       <img src={img} alt='' />
-      <h1>{title}</h1>
+      <h1
+        onClick={() => {
+          alert(title);
+        }}
+      >
+        {title}
+      </h1>
       <h4>{author}</h4>
+      <button type='button' onClick={clickHandler()}>
+        Reference example
+      </button>
+      <button type='button' onClick={() => complexExample(author)}>
+        Complex example
+      </button>
     </article>
   );
 };
