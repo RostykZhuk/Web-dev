1b4942bfc2e8f63d70f8e20399ee6e1091cdfffa
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-01-08 17:25:39 +0200

    props

--- React-Youtube/FreeCodeCamp Course1/my-book/src/index.js
+++ React-Youtube/FreeCodeCamp Course1/my-book/src/index.js
@@ -3,26 +3,40 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 
 // css
-
+const firstBook = {
+  img: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1586722975l/2767052._SY475_.jpg',
+  title: 'The Hunger Games',
+  author: 'Suzanne Collins',
+};
+const secondBook = {
+  img: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1660911061l/2.jpg',
+  title: 'Harry Potter and the Order of the Phoenix',
+  author: ' J.K. Rowling, Mary GrandPré',
+};
 function BookList() {
   return (
     <section className='booklist'>
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const author = 'Suzanne Collins';
-const Book = () => {
-  const title = 'The Hunger Games';
+const Book = (props) => {
+  console.log(props);
   return (
     <article className='book'>
-      <img
-        src='https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1586722975l/2767052._SY475_.jpg'
-        alt=''
-      />
-      <h1>{title}</h1>
-      <h4>{author.toUpperCase()}</h4>
+      <img src={props.img} alt='' />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   );
 };
