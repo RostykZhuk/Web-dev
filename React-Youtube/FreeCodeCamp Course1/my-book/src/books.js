9f99006e77289cf94ad8ed175ff6bb4034e3f074
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-01-08 22:52:18 +0200

    completed booklist

--- React-Youtube/FreeCodeCamp Course1/my-book/src/books.js
+++ React-Youtube/FreeCodeCamp Course1/my-book/src/books.js
@@ -10,12 +10,30 @@ export const books = [
     id: 2,
     img: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1660911061l/2.jpg',
     title: 'Harry Potter and the Order of the Phoenix',
-    author: ' J.K. Rowling, Mary GrandPré',
+    author: 'J.K. Rowling, Mary GrandPré',
   },
   {
     id: 3,
     img: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1320399351l/1885.jpg',
     title: 'Pride and Prejudice',
-    author: ' Jane Austen, Anna Quindlen',
+    author: 'Jane Austen, Anna Quindlen',
+  },
+  {
+    id: 4,
+    img: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1553383690l/2657.jpg',
+    title: 'To Kill a Mockingbird',
+    author: 'Harper Lee',
+  },
+  {
+    id: 5,
+    img: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1522157426l/19063._SY475_.jpg',
+    title: 'The Book Thief',
+    author: 'Markus Zusak ',
+  },
+  {
+    id: 6,
+    img: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1361039443l/41865.jpg',
+    title: 'Twilight',
+    author: ' Stephenie Meyer',
   },
 ];
