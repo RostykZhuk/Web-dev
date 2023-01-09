db04ea3d17fa8e7292e4f186dcb35fd35d74b48f
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-01-09 16:52:48 +0200

    usestate simple case

---
+++ React-Youtube/FreeCodeCamp Course1/react-advanced/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Setup from './tutor/1-useState/setup/1-error-example';
+function App() {
+  return (
+    <div className='container'>
+      <Setup />
+    </div>
+  );
+}
+
+export default App;
