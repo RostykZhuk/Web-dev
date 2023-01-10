9c9d2df9568f162e9acfbdf0d63be0978f7b5a52
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-01-10 04:53:14 +0200

    useState array

--- React-Youtube/FreeCodeCamp Course1/react-advanced/src/App.js
+++ React-Youtube/FreeCodeCamp Course1/react-advanced/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Setup from './tutor/1-useState/setup/2-useState-basics';
+import Setup from './tutor/1-useState/setup/3-useState-array';
 function App() {
   return (
     <div className='container'>
