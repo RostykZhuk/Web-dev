a4d7a241082feaa938d91800c83b35bd811e0dd5
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-01-10 13:40:37 +0200

    functional update form

--- React-Youtube/FreeCodeCamp Course1/react-advanced/src/tutor/1-useState/setup/5-useState-counter.js
+++ React-Youtube/FreeCodeCamp Course1/react-advanced/src/tutor/1-useState/setup/5-useState-counter.js
@@ -5,6 +5,15 @@ const UseStateCounter = () => {
   const reset = () => {
     setValue(0);
   };
+
+  const complexIncrease = () => {
+    setTimeout(() => {
+      // setValue(value + 1); //increases only once by clicking multiple times
+      setValue((prevState) => {
+        return prevState + 1;
+      });
+    }, 3000);
+  };
   return (
     <>
       <section style={{ margin: '4rem 0' }}>
@@ -28,6 +37,13 @@ const UseStateCounter = () => {
           }}
         >
           increase
+        </button>
+      </section>
+      <section style={{ margin: '4rem 0' }}>
+        <h2>more complex counter</h2>
+        <h1>{value}</h1>
+        <button className='btn' onClick={complexIncrease}>
+          increase later
         </button>
       </section>
     </>
