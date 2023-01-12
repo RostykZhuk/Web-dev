b64a4a730e2c6423f13c4335bf31ea7bfd1b7668
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-01-12 12:04:18 +0200

    cleanup function

--- React-Youtube/Web Dev Simplified/react-hooks/src/App.js
+++ React-Youtube/Web Dev Simplified/react-hooks/src/App.js
@@ -1,28 +1,16 @@
 import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [resourceType, setResourceType] = useState('posts');
-  const [items, setItems] = useState([]);
+  const [windowWidth, setwindowWidth] = useState(window.innerWidth);
+  const handleResize = () => {
+    setwindowWidth(window.innerWidth);
+  };
 
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-      .then((response) => response.json())
-      .then((json) => setItems(json));
-  }, [resourceType]);
-  // we want to run useEffect if our resourceType changes
-  return (
-    <>
-      <div>
-        <button onClick={() => setResourceType('posts')}>Posts</button>
-        <button onClick={() => setResourceType('users')}>Users</button>
-        <button onClick={() => setResourceType('comments')}>Comments</button>
-      </div>
-      <h1>{resourceType}</h1>
-      {items.map((item) => {
-        return <pre>{JSON.stringify(item)}</pre>;
-      })}
-    </>
-  );
+    window.addEventListener('resize', handleResize);
+    return () => [window.removeEventListener('resize', handleResize)];
+  }, []);
+  return <div>{windowWidth}</div>;
 }
 
 export default App;
