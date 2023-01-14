40655c90e1dbf019ec1e5b21b3ecd459cb647064
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-01-14 21:25:27 +0200

    displaying tour component

--- React-Youtube/FreeCodeCamp Course1/react-pet-proj/02-tours/src/Tours.js
+++ React-Youtube/FreeCodeCamp Course1/react-pet-proj/02-tours/src/Tours.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+const Tours = ({ tours }) => {
+  return (
+    <section className='title'>
+      <h2>Tours</h2>
+      <div className='underline'></div>
+      <div>
+        {tours.map((tour) => {
+          // using spread operator to get all properties from api
+          return <Tour key={tour.id} {...tour} />;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Tours;
