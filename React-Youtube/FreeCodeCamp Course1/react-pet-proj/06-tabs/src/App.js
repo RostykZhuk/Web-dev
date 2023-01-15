f9fee49969dfaec65d1ce7a93c38528387f11c1b
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-01-15 20:59:21 +0200

    displaying the first job

--- React-Youtube/FreeCodeCamp Course1/react-pet-proj/06-tabs/src/App.js
+++ React-Youtube/FreeCodeCamp Course1/react-pet-proj/06-tabs/src/App.js
@@ -28,7 +28,31 @@ function App() {
     );
   }
 
-  return <h2>jobs</h2>;
+  const { company, dates, duties, title } = jobs[value];
+
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>experience</h2>
+        <div className='underline'></div>
+      </div>
+      <div className='jobs-center'>
+        <article className='job-info'>
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className='job-date'>{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className='job-desc'>
+                <FaAngleDoubleRight className='job-icon' />
+                <p>{duty}</p>
+              </div>
+            );
+          })}
+        </article>
+      </div>
+    </section>
+  );
 }
 
 export default App;
