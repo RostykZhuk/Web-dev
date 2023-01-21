d405ae9ffcd3c3cdf2a04322e573ab24cfe90e7a
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-01-21 21:33:50 +0200

    useRef

--- React-Youtube/FreeCodeCamp Course1/react-advanced/src/App.js
+++ React-Youtube/FreeCodeCamp Course1/react-advanced/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Setup from './tutor/4-forms/setup/2-multiple-inputs';
+import Setup from './tutor/5-useRef/setup/1-useRef-basics';
 function App() {
   return (
     <div className='container'>
