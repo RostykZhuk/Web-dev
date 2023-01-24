be24443f06d917382f0717ff9eaae21d4bcd24e2
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-01-24 10:03:17 +0200

    useReducer remove Item

--- React-Youtube/FreeCodeCamp Course1/react-advanced/src/tutor/6-useReducer/setup/index.js
+++ React-Youtube/FreeCodeCamp Course1/react-advanced/src/tutor/6-useReducer/setup/index.js
@@ -16,6 +16,16 @@ const reducer = (state, action) => {
   if (action.type === 'NO_VALUE') {
     return { ...state, isModalOpen: true, modalContent: 'please enter value' };
   }
+  if (action.type === 'CLOSE_MODAL') {
+    return { ...state, isModalOpen: false };
+  }
+
+  if (action.type === 'REMOVE_ITEM') {
+    const newPeople = state.people.filter(
+      (person) => person.id !== action.payload
+    );
+    return { ...state, people: newPeople };
+  }
   throw new Error('no matching action type');
 };
 const defaultState = {
@@ -37,9 +47,16 @@ const Index = () => {
       dispatch({ type: 'NO_VALUE' });
     }
   };
+
+  const closeModal = () => {
+    dispatch({ type: 'CLOSE_MODAL' });
+  };
+
   return (
     <>
-      {state.isModalOpen && <Modal modalContent={state.modalContent} />}
+      {state.isModalOpen && (
+        <Modal closeModal={closeModal} modalContent={state.modalContent} />
+      )}
       <form onSubmit={handleSubmit} className='form'>
         <div>
           <input
@@ -52,8 +69,15 @@ const Index = () => {
       </form>
       {state.people.map((person) => {
         return (
-          <div key={person.id}>
+          <div key={person.id} className='item'>
             <h4>{person.name}</h4>
+            <button
+              onClick={() =>
+                dispatch({ type: 'REMOVE_ITEM', payload: person.id })
+              }
+            >
+              remove
+            </button>
           </div>
         );
       })}
