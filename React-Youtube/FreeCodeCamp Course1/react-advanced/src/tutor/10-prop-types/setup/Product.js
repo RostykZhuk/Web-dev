88c95e6915e0dc38e1820e9670e47c2d69e332e0
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-01-27 10:15:40 +0200

    proptypes default values

--- React-Youtube/FreeCodeCamp Course1/react-advanced/src/tutor/10-prop-types/setup/Product.js
+++ React-Youtube/FreeCodeCamp Course1/react-advanced/src/tutor/10-prop-types/setup/Product.js
@@ -2,12 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import defaultImage from '../../../assets/default-image.jpeg';
 const Product = ({ image, name, price }) => {
+  const url = image && image.url;
   return (
     <article className='product'>
       <h4>single product</h4>
-      <img src={image.url} alt={name} />
+      <img src={url || defaultImage} alt={name || 'default name'} />
       <h4>{name}</h4>
-      <p>{price}</p>
+      <p>{price || 3.99}</p>
     </article>
   );
 };
