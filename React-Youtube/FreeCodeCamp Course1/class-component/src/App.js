4d430f0cf758be6c1151821ead548077c490ca2e
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-02-03 18:45:58 +0200

    commonly used lifecycle methods

---
+++ React-Youtube/FreeCodeCamp Course1/class-component/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { ReactDOM } from 'react';
+import Counter from './Counter';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mount: true,
+    };
+
+    this.mountCounter = () => this.setState({ mount: true });
+    this.ummountCounter = () => this.setState({ mount: false });
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.mountCounter} disabled={this.state.mount}>
+          Mount Counter
+        </button>
+        <button onClick={this.ummountCounter} disabled={!this.state.mount}>
+          Unmount Counter
+        </button>
+        {this.state.mount ? <Counter /> : null}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ React-Youtube/FreeCodeCamp Course1/class-component/src/Counter.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+class Counter extends React.Component {
+  constructor(props) {
+    console.log('Constructor');
+    super(props);
+
+    this.state = {
+      counter: 0,
+    };
+    this.increment = () => this.setState({ counter: this.state.counter + 1 });
+
+    this.decrement = () => this.setState({ counter: this.state.counter - 1 });
+  }
+
+  componentDidMount() {
+    console.log('Component did Mount');
+    console.log('----------------');
+  }
+
+  render() {
+    console.log('Render');
+
+    return (
+      <div>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+        <div className='counter'>Counter: {this.state.counter}</div>
+      </div>
+    );
+  }
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('Component did update');
+    console.log('----------------');
+  }
+
+  componentWillUnmount() {
+    console.log('Component will Unmount');
+  }
+}
+
+export default Counter;
