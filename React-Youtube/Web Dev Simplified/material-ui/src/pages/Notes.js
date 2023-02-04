59b52a83780dc34123a635acba30c9e5a92482c4
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-02-04 15:27:12 +0200

    Button

--- React-Youtube/Web Dev Simplified/material-ui/src/pages/Notes.js
+++ React-Youtube/Web Dev Simplified/material-ui/src/pages/Notes.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Typography } from '@mui/material';
-
+import { Button } from '@mui/material';
+import { Container } from '@mui/material';
 export default function Notes() {
   return (
-    <div>
+    <Container>
       <Typography
         variant='h6'
         color='textSecondary'
@@ -12,6 +13,25 @@ export default function Notes() {
       >
         Create a New Note
       </Typography>
-    </div>
+
+      <Button
+        onClick={() => console.log('You clicked me')}
+        type='submit'
+        color='secondary'
+        variant='contained'
+      >
+        Submit
+      </Button>
+      {/* <Button type='submit'>Submit</Button>
+      <Button type='submit' color='secondary' variant='outlined'>
+        Submit
+      </Button> */}
+
+      {/* <ButtonGroup color='secondary' variant='contained'>
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup> */}
+    </Container>
   );
 }
