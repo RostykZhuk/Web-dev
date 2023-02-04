e0e604997c887814c6ff768cf168e4daf515b343
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-02-04 16:50:00 +0200

    icons

--- React-Youtube/Web Dev Simplified/material-ui/src/pages/Create.js
+++ React-Youtube/Web Dev Simplified/material-ui/src/pages/Create.js
@@ -1,10 +1,42 @@
 import React from 'react';
 import { Typography } from '@mui/material';
+import { Button } from '@mui/material';
+import { Container } from '@mui/material';
+import { AcUnitOutlined } from '@mui/icons-material';
+import SendIcon from '@mui/icons-material/Send';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 export default function Create() {
   return (
-    <div>
-      <Typography>Create a New Note</Typography>
-    </div>
+    <Container>
+      <Typography
+        variant='h6'
+        color='textSecondary'
+        component='h2'
+        gutterBottom
+      >
+        Create a New Note
+      </Typography>
+
+      <Button
+        onClick={() => console.log('You clicked me')}
+        type='submit'
+        color='secondary'
+        variant='contained'
+        startIcon={<SendIcon />}
+        endIcon={<KeyboardArrowRightIcon />}
+      >
+        Submit
+      </Button>
+
+      {/* icons */}
+      {/* <br />
+      <AcUnitOutlined />
+      <AcUnitOutlined color='secondary' fontSize='large' />
+      <AcUnitOutlined color='secondary' fontSize='small' />
+      <AcUnitOutlined color='action' fontSize='small' />
+      <AcUnitOutlined color='error' fontSize='small' />
+      <AcUnitOutlined color='disabled' fontSize='small' /> */}
+    </Container>
   );
 }
--- React-Youtube/Web Dev Simplified/material-ui/src/pages/Notes.js
+++ React-Youtube/Web Dev Simplified/material-ui/src/pages/Notes.js
@@ -1,37 +1,7 @@
 import React from 'react';
-import { Typography } from '@mui/material';
-import { Button } from '@mui/material';
-import { Container } from '@mui/material';
-export default function Notes() {
-  return (
-    <Container>
-      <Typography
-        variant='h6'
-        color='textSecondary'
-        component='h2'
-        gutterBottom
-      >
-        Create a New Note
-      </Typography>
 
-      <Button
-        onClick={() => console.log('You clicked me')}
-        type='submit'
-        color='secondary'
-        variant='contained'
-      >
-        Submit
-      </Button>
-      {/* <Button type='submit'>Submit</Button>
-      <Button type='submit' color='secondary' variant='outlined'>
-        Submit
-      </Button> */}
-
-      {/* <ButtonGroup color='secondary' variant='contained'>
-        <Button>One</Button>
-        <Button>Two</Button>
-        <Button>Three</Button>
-      </ButtonGroup> */}
-    </Container>
-  );
+function Notes() {
+  return <div>Notes</div>;
 }
+
+export default Notes;
