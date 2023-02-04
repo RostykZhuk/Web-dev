e45112bd308c3b910c4119304d1670aa5f79797f
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-02-04 17:00:58 +0200

    Route specifice

--- React-Youtube/Web Dev Simplified/react-router/src/pages/BookList.js
+++ React-Youtube/Web Dev Simplified/react-router/src/pages/BookList.js
@@ -5,7 +5,10 @@ function BookList() {
     <>
       <h1>BookList</h1>
       <Link to='/books/1'>Book1</Link>
+      <br />
       <Link to='/books/2'>Book2</Link>
+      <br />
+      <Link to='/books/new'>New Book</Link>
     </>
   );
 }
