ed4e56f7a836ea8dd69b0950eb42b6c609b0a8fb
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-02-26 15:58:06 +0200

    clear cart

--- React-Youtube/Redux Toolkit/FreecodeCamp/src/components/CartContainer.js
+++ React-Youtube/Redux Toolkit/FreecodeCamp/src/components/CartContainer.js
@@ -1,7 +1,9 @@
 import CartItems from './CartItems';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearCart } from '../features/cart/cartSlice';
 
 const CartContainer = () => {
+  const dispatch = useDispatch();
   const { cartItems, total, amount } = useSelector((store) => store.cart);
   if (amount < 1) {
     return (
@@ -30,7 +32,9 @@ const CartContainer = () => {
             total <span>${total}</span>
           </h4>
         </div>
-        <button className='btn clear-btn'>clear cart</button>
+        <button className='btn clear-btn' onClick={() => dispatch(clearCart())}>
+          clear cart
+        </button>
       </footer>
     </section>
   );
