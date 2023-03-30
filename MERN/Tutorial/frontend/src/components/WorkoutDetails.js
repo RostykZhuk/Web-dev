f5b1900b9e227128296ac3e63fe156196905a965
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-03-30 14:01:36 +0300

    Finishing Touches

--- MERN/Tutorial/frontend/src/components/WorkoutDetails.js
+++ MERN/Tutorial/frontend/src/components/WorkoutDetails.js
@@ -1,5 +1,8 @@
 import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
 
+// date fns
+import formatDistanceToNow from 'date-fns/formatDistanceToNow';
+
 function WorkoutDetails({ workout }) {
   const { dispatch } = useWorkoutsContext();
 
@@ -24,8 +27,12 @@ function WorkoutDetails({ workout }) {
         <strong>Reps: </strong>
         {workout.reps}
       </p>
-      <p>{workout.createdAt}</p>
-      <span onClick={handleClick}>delete</span>
+      <p>
+        {formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}
+      </p>
+      <span className='material-symbols-outlined' onClick={handleClick}>
+        delete
+      </span>
     </div>
   );
 }
