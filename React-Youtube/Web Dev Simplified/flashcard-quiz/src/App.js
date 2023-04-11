adc57b9c66729944c02be3cde75cf7c5abde2fea
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-04-11 17:21:14 +0300

    hold data

---
+++ React-Youtube/Web Dev Simplified/flashcard-quiz/src/App.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import FlashcardList from './FlashcardList';
+function App() {
+  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
+  return <FlashcardList flashcards={flashcards} />;
+}
+
+export default App;
+
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: 'What is 2+2?',
+    answer: '4',
+    options: ['2', '3', '4', '5'],
+  },
+  {
+    id: 2,
+    question: 'Question 2',
+    answer: 'Answer',
+    options: ['2', '3', '4', '5'],
+  },
+];
