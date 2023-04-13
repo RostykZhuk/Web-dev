71e65eab9d79c83ae6cceb802ce6c4cd1829abc1
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-04-13 16:21:02 +0300

    firebase connection

--- CRM-authorisation/proj/src/index.js
+++ CRM-authorisation/proj/src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import { store } from 'store';
+import './firebase';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
