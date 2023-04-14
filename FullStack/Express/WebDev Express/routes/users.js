9492ecafb9315a94b9d535ab4d814fec6fec322c
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-04-14 22:32:26 +0300

    advanced routing

--- FullStack/Express/WebDev Express/routes/users.js
+++ FullStack/Express/WebDev Express/routes/users.js
@@ -7,4 +7,28 @@ router.get('/', (req, res) => {
 router.get('/new', (req, res) => {
   res.send('User New Form');
 });
+
+router.post('/', (req, res) => {
+  res.send('Create User');
+});
+
+router
+  .route('/:id')
+  .get((req, res) => {
+    console.log(req.user);
+    res.send(`Get user with id ${req.params.id}`);
+  })
+  .put((req, res) => {
+    res.send(`Get user with id ${req.params.id}`);
+  })
+  .delete((req, res) => {
+    res.send(`Get user with id ${req.params.id}`);
+  });
+
+const users = [{ name: 'Rostyk' }, { name: 'Martin' }];
+router.param('id', (req, res, next, id) => {
+  req.user = users[id];
+  next();
+});
+
 module.exports = router;
