ff44a3923171b3491ba15b3860902fd5da77fbc0
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-04-21 11:16:24 +0300

    finding a single document

--- FullStack/Mongo/Mongo Course/app.js
+++ FullStack/Mongo/Mongo Course/app.js
@@ -1,23 +1,26 @@
 const express = require('express');
-const { connectToDb, getDb } = require('./db');
+const { getDb, connectToDb } = require('./db');
+const { ObjectId } = require('mongodb');
 
-//init app and middleware
+// init app & middleware
 const app = express();
 
 // db connection
 let db;
+
 connectToDb((err) => {
   if (!err) {
-    app.listen(3000, () => {
+    app.listen('3000', () => {
       console.log('app listening on port 3000');
     });
     db = getDb();
   }
 });
 
-//routes
+// routes
 app.get('/books', (req, res) => {
   let books = [];
+
   db.collection('books')
     .find()
     .sort({ author: 1 })
@@ -28,5 +31,19 @@ app.get('/books', (req, res) => {
     .catch(() => {
       res.status(500).json({ error: 'Could not fetch the documents' });
     });
-  res.json({ mssg: 'welcome to the api' });
+});
+
+app.get('/books/:id', (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection('books')
+      .findOne({ _id: new ObjectId(req.params.id) })
+      .then((doc) => {
+        res.status(200).json(doc);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: 'Could not fetch the document' });
+      });
+  } else {
+    res.status(500).json({ error: 'Could not fetch the document' });
+  }
 });
