1f64571eff3d0b28e4ce1bc88655784111d7d911
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-05-01 20:55:16 +0300

    seo+meta

--- Next/next-course1/pages/index.js
+++ Next/next-course1/pages/index.js
@@ -1,8 +1,15 @@
 import Link from 'next/link';
+import Head from 'next/head';
 
 export default function Index() {
   return (
     <>
+      <Head>
+        <title>Next Title</title>
+        <meta name='keywords' content='next,js,next react' />
+        <meta name='description' content='tryying out' />
+        <meta charSet='utf-8' content='tryying out' />
+      </Head>
       <h1>Hello Next JS</h1>
       <p>
         <Link href={'/about'}>About</Link>
--- Next/next-course1/pages/posts.js
+++ Next/next-course1/pages/posts.js
@@ -1,6 +1,11 @@
+import Head from 'next/head';
+
 export default function Posts() {
   return (
     <>
+      <Head>
+        <title>Post Page | Next Course</title>
+      </Head>
       <h1>Posts Page</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
     </>
