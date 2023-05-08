e82b5088fd45079b2e494b188a134517dc752d7b
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-05-08 11:29:32 +0300

    Build The Input Todo Component

---
+++ PERN/TODO/client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import InputTodo from './components/InputTodo';
+
+function App() {
+  return (
+    <>
+      <div className='container'>
+        <InputTodo />
+      </div>
+    </>
+  );
+}
+
+export default App;
