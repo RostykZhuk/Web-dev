c725f5c3a146538b3db648b223f86bf6e3bf0c95
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-05-08 12:38:46 +0300

    Build the delete functionality

--- PERN/TODO/client/src/components/ListTodo.js
+++ PERN/TODO/client/src/components/ListTodo.js
@@ -1,23 +1,42 @@
 import React, { useEffect, useState } from 'react';
+
 import EditTodo from './EditTodo';
-function ListTodo() {
+
+const ListTodos = () => {
   const [todos, setTodos] = useState([]);
-  const getTodos = async () => {
+
+  //delete todo function
+
+  const deleteTodo = async (id) => {
     try {
-      const resp = await fetch('http://localhost:5000/todos');
-      const jsonData = await resp.json();
-      setTodos(jsonData);
-    } catch (error) {
-      console.error(error.message);
+      const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+        method: 'DELETE',
+      });
+
+      setTodos(todos.filter((todo) => todo.todo_id !== id));
+    } catch (err) {
+      console.error(err.message);
     }
   };
+
   useEffect(() => {
+    const getTodos = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/todos');
+        const jsonData = await response.json();
+
+        setTodos(jsonData);
+      } catch (err) {
+        console.error(err.message);
+      }
+    };
     getTodos();
   }, []);
+  console.log(todos);
+
   return (
-    <div>
-      {' '}
-      <table className='table mt-5 text-center'>
+    <>
+      <table class='table mt-5 text-center'>
         <thead>
           <tr>
             <th>Description</th>
@@ -35,7 +54,7 @@ function ListTodo() {
               <td>
                 <button
                   className='btn btn-danger'
-                  // onClick={() => deleteTodo(todo.todo_id)}
+                  onClick={() => deleteTodo(todo.todo_id)}
                 >
                   Delete
                 </button>
@@ -44,8 +63,8 @@ function ListTodo() {
           ))}
         </tbody>
       </table>
-    </div>
+    </>
   );
-}
+};
 
-export default ListTodo;
+export default ListTodos;
