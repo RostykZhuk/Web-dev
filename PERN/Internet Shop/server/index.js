8c7adce640f232a72b2e9d217ade868bcfde391d
Rostyk Zhuk <rostukzhuk@gmail.com>
2023-05-21 11:14:53 +0300

    filtering getting devices

--- PERN/Internet Shop/server/index.js
+++ PERN/Internet Shop/server/index.js
@@ -1,15 +1,18 @@
 require('dotenv').config();
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const sequelize = require('./db');
 const models = require('./models/models');
 const cors = require('cors');
-const PORT = process.env.PORT || 5000;
 const router = require('./routes/index');
 const errorHandler = require('./middleware/ErrorHandlingMiddleware');
-
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(fileUpload({}));
 app.use('/api', router);
 // Обработка ошибок, последний Middleware
 app.use(errorHandler);
